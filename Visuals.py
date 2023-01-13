14072a2d7f0a0a389fc29c3d6a9f131695ddeba6
Howard <howard@hkw.org.uk>
2023-01-13 13:34:25 +0000

    Speed calcs added for visual

--- Visuals.py
+++ Visuals.py
@@ -41,16 +41,27 @@ def create_overall_summary():
 def create_daily_charts(dir):
 
     for df_file in Path(dir).rglob('*.csv.gz'):
-        render_time_series_chart(df_file,  'Hamble', 'Windhover', [('06:00:00', '19:00:00')])
-        render_time_series_chart(df_file,  'Hound', 'Mallards', [('07:00:00', '09:00:00'), ('14:00:00', '16:00:00')])
+        render_time_series_chart(df_file, 'Hamble', 'Windhover', 'speed_mph',        [('06:00:00', '19:00:00')])
+        render_time_series_chart(df_file, 'Hamble', 'Windhover', 'duration_minutes', [('06:00:00', '19:00:00')])
+        render_time_series_chart(df_file, 'Hound',  'Mallards',  'speed_mph',        [('07:00:00', '09:00:00'), ('14:00:00', '16:00:00')])
+        render_time_series_chart(df_file, 'Hound',  'Mallards',  'duration_minutes', [('07:00:00', '09:00:00'), ('14:00:00', '16:00:00')])
+
 
 # Creates a chart from agiven df and saves the rendering of it
-def render_time_series_chart(df_file_name, origin, dest, time_groups=None):
+def render_time_series_chart(df_file_name, origin, dest, measure_and_unit='duration_minutes', time_groups=None):
     
+    if measure_and_unit not in ['duration_minutes', 'speed_mph']:
+        return
+
+    MS_TO_MPH = 2.2369362912
+    # Cemex/i-Transport average based on 34mph average speed on Hamble lane...?
+    # Google api/distanceMatrix distance average by Cemex speed average 34mph
+    REPORTED_MEAN_SPEED = 15.1994 # m/s
+
     date = datetime.strptime(os.path.basename(df_file_name).split('.')[0], '%Y-%m-%d')
-
-    fig_name = f'{origin}-{dest}-{str(date.date())}-ts.jpg'
-
+    measure = measure_and_unit.split('_')[0]
+    unit = measure_and_unit.split('_')[1]
+    fig_name = f'{str(date.date())}-{origin}-{dest}-{measure}-ts.jpg'
     year = date.strftime('%Y')
     month = date.strftime('%b')
     
@@ -60,11 +71,12 @@ def render_time_series_chart(df_file_name, origin, dest, time_groups=None):
     if not os.path.isfile(fig_file_handle) or (os.path.isfile(fig_file_handle) and datetime.now().date() == date.date()):
         
         df = pd.read_csv(df_file_name)
-
+        
         # Add a new column for journey time in minutes
-        df['duration_mins'] = df['duration']/60
-        # Add in speed/velocity
+        df['duration_minutes'] = df['duration']/60
+        # Add in speed/velocity in m/s
         df['speed'] = df['distance']/df['duration']
+        df['speed_mph'] = df['speed'] * MS_TO_MPH
         df['timestamp'] = pd.to_datetime(df['timestamp'])
         #df['timestamp'] = df['timestamp'].apply( lambda t : t.time())
 
@@ -89,39 +101,36 @@ def render_time_series_chart(df_file_name, origin, dest, time_groups=None):
         o2d_label = f"{origin} to {dest}"
         d20_label = f"{dest} to {origin}"
 
-        plt.plot(o2d['duration_mins'], linestyle='solid', color="green", label=f'{o2d_label} actuals')
-        plt.plot(d2o['duration_mins'], linestyle='solid', color="blue", label=f'{d20_label} actuals')
+        plt.plot(o2d[measure_and_unit], linestyle='solid', color="green", label=f'{o2d_label} actuals')
+        plt.plot(d2o[measure_and_unit], linestyle='solid', color="blue", label=f'{d20_label} actuals')
 
         # Daily average
-        # Cemex/i-Transport average based on 34mph average speed on Hamble lane...?
-        # Google api/distanceMatrix distance average by Cemex speed average 34mph
-        REPORTED_MEAN_SPEED = 15.1994 # m/s
-
-        report_average_mins = (statistics.fmean([o2d['distance'].mean(), d2o['distance'].mean()])/REPORTED_MEAN_SPEED) / 60.0
- 
+        if measure_and_unit == 'duration_minutes':
+            report_average = (statistics.fmean([o2d[measure_and_unit].mean(), d2o[measure_and_unit].mean()])/REPORTED_MEAN_SPEED)
+        else:
+            report_average = REPORTED_MEAN_SPEED * MS_TO_MPH
         #print(report_average_mins)
 
-        plt.axhline(y=report_average_mins, linestyle="dotted", color='red', label=f'Cemex/i-Transport reported average') 
-        plt.axhline(y=o2d['duration_mins'].mean(), linestyle='dotted', color='green', label=f'{o2d_label} full day average')
-        plt.axhline(y=d2o['duration_mins'].mean(), linestyle='dotted', color='blue', label=f'{d20_label} full day average')
+        plt.axhline(y=report_average, linestyle="dotted", color='red', label=f'Cemex/i-Transport reported average') 
+        plt.axhline(y=o2d[measure_and_unit].mean(), linestyle='dotted', color='green', label=f'{o2d_label} full day average')
+        plt.axhline(y=d2o[measure_and_unit].mean(), linestyle='dotted', color='blue', label=f'{d20_label} full day average')
 
         # Actual avarages when most people are out and about and observe delays
         line_styles = ['dashed', 'dashdot']
-        # TODO: Add on average speeds m/s to mph
 
         for index, (start_time, end_time) in enumerate(time_groups):
             if start_time is not None and end_time is not None:
                 plt.axhline(
-                    y=o2d.between_time(start_time, end_time).mean(numeric_only=True)['duration_mins'], 
+                    y=o2d.between_time(start_time, end_time).mean(numeric_only=True)[measure_and_unit], 
                     linestyle=line_styles[index], color="green", label=f'{o2d_label} average {start_time} and {end_time}')
 
                 plt.axhline(
-                    y=d2o.between_time(start_time, end_time).mean(numeric_only=True)['duration_mins'], 
+                    y=d2o.between_time(start_time, end_time).mean(numeric_only=True)[measure_and_unit], 
                     linestyle=line_styles[index], color="blue", label=f'{d20_label} average {start_time} and {end_time}')
         
-        plt.title(f'Journey times beteen {origin} and {dest} for {date.date()}')
+        plt.title(f'Journey {measure} beteen {origin} and {dest} for {date.date()}')
         plt.xlabel('Time')
-        plt.ylabel('Duration (minutes)')
+        plt.ylabel(f'{measure}  ({unit})')
         plt.legend()
         plt.grid()
         plt.legend(facecolor='lightgrey', framealpha=1)
